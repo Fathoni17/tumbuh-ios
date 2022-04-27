ba16be9dcdcca59ad1c28b0fb07a7c15d8286929
Fathoni <mfathonin@gmail.com>
2022-04-28 05:19:38 +0700

    Add Tab bar

--- tumbuh/AccountsVC.swift
+++ tumbuh/AccountsVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class AccountVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ tumbuh/GoalsVC.swift
@@ -0,0 +1,29 @@
+//
+//  GoalsVC.swift
+//  tumbuh
+//
+//  Created by Fathoni on 28/04/22.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
