dfb4c5786bc91b9fbf474f1c5d1a9da89417fd5a
Fathoni <mfathonin@gmail.com>
2022-05-07 14:49:34 +0700

    Add accountItemCell to account page

--- tumbuh/Controller/AccountsVC.swift
+++ tumbuh/Controller/AccountsVC.swift
@@ -8,16 +8,40 @@
 import UIKit
 
 class AccountVC: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        registerCell()
+        tableView.separatorStyle = .none
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        setupNavigationBar()
     }
 
-    override func viewWillAppear(_ animated: Bool) {
+    func registerCell() {
+        tableView.register(UINib(nibName: "AccountItemCell", bundle: nil), forCellReuseIdentifier: "accountItemCellId")
+    }
+    
+    func setupNavigationBar() {
         self.navigationController?.navigationBar.prefersLargeTitles = true
         self.tabBarController?.navigationItem.title = "Accounts"
     }
-
 }
 
+extension AccountVC: UITableViewDelegate {
+}
+
+extension AccountVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = (tableView.dequeueReusableCell(withIdentifier: "accountItemCellId", for: indexPath) as? AccountItemCell)!
+        
+        return cell
+    }
+}
